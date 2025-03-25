7b3251e4b11e36495d91fc56b21b42e41dfccf76
andres.mera <feli041089@gmail.com>
2025-03-25 11:00:05 -0500

    code 01: Adding the Side Menu and the base components of the app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'customer', component: CustomerComponent },
+  { path: 'schedule', component: ScheduleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,38 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { HomeComponent } from './components/home/home.component';
+import { BookComponent } from './components/book/book.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    HomeComponent,
+    BookComponent,
+    CustomerComponent,
+    ScheduleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
